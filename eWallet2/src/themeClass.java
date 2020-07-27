4211c2f471c7f78b5feb09223c41ca31be51fd86
Devjyot Singh Sidhu <devjyotsinhsidhu@gmail.com>
2020-07-28 01:08:50 +0530

    Critical bug fix

--- eWallet2/src/themeClass.java
+++ eWallet2/src/themeClass.java
@@ -2,6 +2,7 @@ import java.util.prefs.Preferences;
 public class themeClass {
     Preferences pref=Preferences.userRoot().node(this.getClass().getName());
     public void setPref(int theme){
+        pref.getInt("value",0);
         pref.putInt("value", theme);
     }
     public int getPref(){
